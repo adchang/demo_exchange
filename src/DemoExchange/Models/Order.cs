0d3c8e85deb6ddfdcc7aad2defa37d7571bfb1b9
adchang <sprezzarete.loki@changme.net>
2020-10-01 11:54:05 +0700

    Add OrderEntity and preconditions for Order

--- src/DemoExchange/Models/Order.cs
+++ src/DemoExchange/Models/Order.cs
@@ -1,15 +1,20 @@
 using System;
+using static Utils.Preconditions;
 
 namespace DemoExchange.Models {
+  /// <summary>
+  /// Base Class representing an Order.
+  /// </summary>
+  ///
   public abstract class Order {
     const int TIME_IN_FORCE_TO_BE_CANCELLED_DAYS = 90;
 
-    public String Id { get; }
+    public String Id { get; protected set; }
     public long CreatedTimestamp { get; protected set; }
     public DateTime CreatedDateTime {
       get { return new DateTime(CreatedTimestamp); }
     }
-    public String AccountId { get; }
+    public String AccountId { get; protected set; }
     public OrderStatus Status { get; set; }
     public bool IsOpen {
       get { return Status.Equals(OrderStatus.OPEN); }
@@ -26,15 +31,15 @@ namespace DemoExchange.Models {
     public bool IsDeleted {
       get { return Status.Equals(OrderStatus.DELETED); }
     }
-    public OrderAction Action { get; }
+    public OrderAction Action { get; protected set; }
     public bool IsBuyOrder {
       get { return Action.Equals(OrderAction.BUY); }
     }
     public bool IsSellOrder {
       get { return Action.Equals(OrderAction.SELL); }
     }
-    public String Ticker { get; }
-    public OrderType Type { get; }
+    public String Ticker { get; protected set; }
+    public OrderType Type { get; protected set; }
     public bool IsMarketOrder {
       get { return Type.Equals(OrderType.MARKET); }
     }
@@ -59,36 +64,51 @@ namespace DemoExchange.Models {
     public bool IsImmediateOrCancelOrder {
       get { return Type.Equals(OrderType.IMMEDIATE_OR_CANCEL); }
     }
-    public decimal StrikePrice { get; set; }
-    public int Quantity { get; }
+    public int Quantity { get; protected set; }
     public int OpenQuantity { get; set; }
     public bool IsFilled {
       get { return OpenQuantity == 0; }
     }
-    public OrderTimeInForce TimeInForce { get; }
+    public decimal StrikePrice { get; set; }
+    public OrderTimeInForce TimeInForce { get; protected set; }
     public bool IsDayOrder {
       get { return TimeInForce.Equals(OrderTimeInForce.DAY); }
     }
     public bool IsGoodTillCanceledOrder {
       get { return TimeInForce.Equals(OrderTimeInForce.GOOD_TIL_CANCELED); }
     }
-    public long ToBeCanceledTimestamp { get; }
+    public long ToBeCanceledTimestamp { get; protected set; }
     public DateTime ToBeCanceledDateTime {
       get { return new DateTime(ToBeCanceledTimestamp); }
     }
 
-    public Order(String accountId, OrderAction action, String ticker, OrderType orderType,
-      decimal strikePrice, int quantity, OrderTimeInForce timeInForce) {
+    protected Order() { }
+
+    public Order(String accountId, OrderAction action, String ticker, OrderType type,
+      int quantity, decimal strikePrice, OrderTimeInForce timeInForce) {
+      CheckNotNullOrWhitespace(accountId, paramName: "AccountId");
+      CheckNotNullOrWhitespace(ticker, paramName: "Ticker");
+      CheckArgument(quantity > 0, message: "Quantity must be greater than 0");
+      if (OrderType.MARKET.Equals(type)) {
+        if (strikePrice != 0) {
+          throw new ArgumentException("StrikePrice should be 0 for Market orders");
+        }
+      } else {
+        if (strikePrice <= 0) {
+          throw new ArgumentException("StrikePrice must be greater than 0");
+        }
+      }
+
       Id = Guid.NewGuid().ToString();
       CreatedTimestamp = System.Diagnostics.Stopwatch.GetTimestamp();
       AccountId = accountId;
       Status = OrderStatus.OPEN;
       Action = action;
       Ticker = ticker;
-      Type = orderType;
-      StrikePrice = strikePrice;
+      Type = type;
       Quantity = quantity;
       OpenQuantity = quantity;
+      StrikePrice = strikePrice;
       TimeInForce = timeInForce;
       ToBeCanceledTimestamp = timeInForce.Equals(OrderTimeInForce.DAY) ?
         0 : CreatedTimestamp + (TimeSpan.TicksPerDay * TIME_IN_FORCE_TO_BE_CANCELLED_DAYS);
@@ -102,9 +122,9 @@ namespace DemoExchange.Models {
         "Action: " + Action + ", " +
         "Ticker: " + Ticker + ", " +
         "Type: " + Type + ", " +
-        "StrikePrice: " + StrikePrice + ", " +
         "Quantity: " + Quantity + ", " +
         "OpenQuantity: " + OpenQuantity + ", " +
+        "StrikePrice: " + StrikePrice + ", " +
         "TimeInForce: " + TimeInForce + ", " +
         "ToBeCanceledTimestamp: " + ToBeCanceledTimestamp + ", " +
         "}";
@@ -123,14 +143,79 @@ namespace DemoExchange.Models {
     }
   }
 
-  public class BuyMarketOrder : Order {
-    public BuyMarketOrder(String accountId, String ticker, int quantity) : base(accountId,
-      OrderAction.BUY, ticker, OrderType.MARKET, 0, quantity, OrderTimeInForce.DAY) { }
+  /// <summary>
+  /// For persistence of an Order.
+  /// </summary>
+  ///
+  public class OrderEntity : Order {
+    new public String Id {
+      get { return base.Id; }
+      set { base.Id = value; }
+    }
+    new public long CreatedTimestamp {
+      get { return base.CreatedTimestamp; }
+      set { base.CreatedTimestamp = value; }
+    }
+    new public String AccountId {
+      get { return base.AccountId; }
+      set { base.AccountId = value; }
+    }
+    new public OrderStatus Status {
+      get { return base.Status; }
+      set { base.Status = value; }
+    }
+    new public OrderAction Action {
+      get { return base.Action; }
+      set { base.Action = value; }
+    }
+    new public String Ticker {
+      get { return base.Ticker; }
+      set { base.Ticker = value; }
+    }
+    new public OrderType Type {
+      get { return base.Type; }
+      set { base.Type = value; }
+    }
+    new public int Quantity {
+      get { return base.Quantity; }
+      set { base.Quantity = value; }
+    }
+    new public int OpenQuantity {
+      get { return base.OpenQuantity; }
+      set { base.OpenQuantity = value; }
+    }
+    new public decimal StrikePrice {
+      get { return base.StrikePrice; }
+      set { base.StrikePrice = value; }
+    }
+    new public OrderTimeInForce TimeInForce {
+      get { return base.TimeInForce; }
+      set { base.TimeInForce = value; }
+    }
+    new public long ToBeCanceledTimestamp {
+      get { return base.ToBeCanceledTimestamp; }
+      set { base.ToBeCanceledTimestamp = value; }
+    }
+
+    public OrderEntity() { }
   }
 
+  /// <summary>
+  /// Convenience class to instantiate a Buy Market Order.
+  /// </summary>
+  ///
+  public class BuyMarketOrder : Order {
+    public BuyMarketOrder(String accountId, String ticker, int quantity) : base(accountId,
+      OrderAction.BUY, ticker, OrderType.MARKET, quantity, 0, OrderTimeInForce.DAY) { }
+  }
+
+  /// <summary>
+  /// Convenience class to instantiate a Sell Market Order.
+  /// </summary>
+  ///
   public class SellMarketOrder : Order {
     public SellMarketOrder(String accountId, String ticker, int quantity) : base(accountId,
-      OrderAction.SELL, ticker, OrderType.MARKET, 0, quantity, OrderTimeInForce.DAY) { }
+      OrderAction.SELL, ticker, OrderType.MARKET, quantity, 0, OrderTimeInForce.DAY) { }
   }
 
   public enum OrderStatus {
--- src/DemoExchangeTest/Models/OrderTest.cs
+++ src/DemoExchangeTest/Models/OrderTest.cs
@@ -1,10 +1,105 @@
 using System;
-using Microsoft.VisualBasic;
 using Xunit;
 
 namespace DemoExchange.Models {
   public class OrderTest {
     [Fact]
+    [Trait("Category", "Unit")]
+    public void OrderTest_constructor() {
+      OrderEntity test = new OrderEntity();
+      test.Id = "123";
+      Order test2 = (Order)test;
+      Assert.Equal("123", test.Id);
+      Assert.Equal("123", test2.Id);
+
+      Exception e = Assert.Throws<ArgumentException>(() =>
+        new TestOrder(null, null, OrderType.MARKET, 0, 0));
+      Assert.Equal("AccountId is null, empty, or contains only white-space characters",
+        e.Message);
+      e = Assert.Throws<ArgumentException>(() =>
+        new TestOrder("accountId", null, OrderType.MARKET, 0, 0));
+      Assert.Equal("Ticker is null, empty, or contains only white-space characters",
+        e.Message);
+      e = Assert.Throws<ArgumentException>(() =>
+        new TestOrder("accountId", "ERX", OrderType.MARKET, 0, 0));
+      Assert.Equal("Quantity must be greater than 0",
+        e.Message);
+      e = Assert.Throws<ArgumentException>(() =>
+        new TestOrder("accountId", "ERX", OrderType.MARKET, -100, 0));
+      Assert.Equal("Quantity must be greater than 0",
+        e.Message);
+      e = Assert.Throws<ArgumentException>(() =>
+        new TestOrder("accountId", "ERX", OrderType.MARKET, 100, -1));
+      Assert.Equal("StrikePrice should be 0 for Market orders",
+        e.Message);
+      e = Assert.Throws<ArgumentException>(() =>
+        new TestOrder("accountId", "ERX", OrderType.LIMIT, 100, -1));
+      Assert.Equal("StrikePrice must be greater than 0",
+        e.Message);
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void OrderEntityTest() {
+      OrderEntity order = new OrderEntity();
+      order.Id = "Id";
+      order.CreatedTimestamp = 1;
+      order.AccountId = "accountId";
+      order.Status = OrderStatus.OPEN;
+      order.Action = OrderAction.BUY;
+      order.Ticker = "ERX";
+      order.Type = OrderType.MARKET;
+      order.Quantity = 100;
+      order.OpenQuantity = 200;
+      order.StrikePrice = 0;
+      order.TimeInForce = OrderTimeInForce.DAY;
+      order.ToBeCanceledTimestamp = 8;
+      Assert.Equal("Id", order.Id);
+      Assert.Equal(1, order.CreatedTimestamp);
+      Assert.Equal("accountId", order.AccountId);
+      Assert.Equal(OrderStatus.OPEN, order.Status);
+      Assert.Equal(OrderAction.BUY, order.Action);
+      Assert.Equal("ERX", order.Ticker);
+      Assert.Equal(OrderType.MARKET, order.Type);
+      Assert.Equal(100, order.Quantity);
+      Assert.Equal(200, order.OpenQuantity);
+      Assert.Equal(0, order.StrikePrice);
+      Assert.Equal(OrderTimeInForce.DAY, order.TimeInForce);
+      Assert.Equal(8, order.ToBeCanceledTimestamp);
+      Order baseOrder = (Order)order;
+      Assert.Equal("Id", baseOrder.Id);
+      Assert.Equal(1, baseOrder.CreatedTimestamp);
+      Assert.Equal("accountId", baseOrder.AccountId);
+      Assert.Equal(OrderStatus.OPEN, baseOrder.Status);
+      Assert.Equal(OrderAction.BUY, baseOrder.Action);
+      Assert.Equal("ERX", baseOrder.Ticker);
+      Assert.Equal(OrderType.MARKET, baseOrder.Type);
+      Assert.Equal(100, baseOrder.Quantity);
+      Assert.Equal(200, baseOrder.OpenQuantity);
+      Assert.Equal(0, baseOrder.StrikePrice);
+      Assert.Equal(OrderTimeInForce.DAY, baseOrder.TimeInForce);
+      Assert.Equal(8, baseOrder.ToBeCanceledTimestamp);
+      OrderEntity order2 = new OrderEntity();
+      order2.Id = "Id";
+      order2.CreatedTimestamp = 1;
+      order2.AccountId = "accountId";
+      order2.Status = OrderStatus.OPEN;
+      order2.Action = OrderAction.BUY;
+      order2.Ticker = "ERX";
+      order2.Type = OrderType.MARKET;
+      order2.Quantity = 100;
+      order2.OpenQuantity = 200;
+      order2.StrikePrice = 0;
+      order2.TimeInForce = OrderTimeInForce.DAY;
+      order2.ToBeCanceledTimestamp = 8;
+      Assert.False(order == order2);
+      Assert.False(order.Equals(null));
+      Assert.True(order.Equals(order2));
+      Assert.Equal(order.GetHashCode(), order2.GetHashCode());
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
     public void MarketOrderTest() {
       Order order = new BuyMarketOrder("accountId", "ERX", 100);
       Assert.False(String.IsNullOrWhiteSpace(order.Id));
@@ -24,14 +119,14 @@ namespace DemoExchange.Models {
       Assert.False(order.IsTrailingStopLimitOrder);
       Assert.False(order.IsFillOrKillOrder);
       Assert.False(order.IsImmediateOrCancelOrder);
-      Assert.Equal(0, order.StrikePrice);
       Assert.Equal(order.Quantity, order.OpenQuantity);
+      Assert.Equal(0, order.StrikePrice);
       Assert.True(order.IsDayOrder);
       Assert.False(order.IsGoodTillCanceledOrder);
       Assert.Equal(new DateTime(order.ToBeCanceledTimestamp), order.ToBeCanceledDateTime);
       Assert.Equal("{Id: " + order.Id + ", CreatedTimestamp: " + order.CreatedTimestamp + ", " +
         "AccountId: accountId, Status: OPEN, Action: BUY, Ticker: ERX, Type: MARKET, " +
-        "StrikePrice: 0, Quantity: 100, OpenQuantity: 100, " +
+        "Quantity: 100, OpenQuantity: 100, StrikePrice: 0, " +
         "TimeInForce: DAY, ToBeCanceledTimestamp: 0, }",
         order.ToString());
 
@@ -45,9 +140,15 @@ namespace DemoExchange.Models {
       Assert.True(order.IsSellOrder);
       Assert.Equal("ERX", order.Ticker);
       Assert.True(order.IsMarketOrder);
-      Assert.Equal(0, order.StrikePrice);
       Assert.Equal(100, order.Quantity);
+      Assert.Equal(0, order.StrikePrice);
       Assert.True(order.IsDayOrder);
+    }
+
+    class TestOrder : Order {
+      public TestOrder(String accountId, String ticker, OrderType type,
+        int quantity, decimal strikePrice) : base(accountId,
+        OrderAction.BUY, ticker, type, quantity, strikePrice, OrderTimeInForce.DAY) { }
     }
   }
 }
