f9c7ce905c484e9a353d88fedb5cb0a4b4bc3116
adchang <sprezzarete.loki@changme.net>
2020-10-05 11:50:33 +0700

    Add Time

--- src/Utils/Utils.cs
+++ src/Utils/Utils.cs
@@ -1,7 +1,7 @@
 using System;
 
 namespace Utils {
-  public class Utils {
+  public class Strings {
     public static String GetValue(String value) {
       return GetValue(value, "");
     }
@@ -10,7 +10,46 @@ namespace Utils {
       return String.IsNullOrWhiteSpace(value) ? nullEmptyValue : value;
     }
 
-    private Utils() {
+    private Strings() {
+      // Prevent instantiation
+    }
+  }
+
+  public class Time {
+    public const int OFFSET_ICT = 7;
+
+    public static long Now {
+      get { return DateTime.UtcNow.Ticks; }
+    }
+
+    public static long MidnightUtc {
+      get { return DateTime.UtcNow.AddDays(1).Date.Ticks; }
+    }
+
+    public static long MidnightIct {
+      get { return MidnightUtc - (TimeSpan.TicksPerHour * OFFSET_ICT); }
+    }
+
+    public static long MidnightSaturdayUtc {
+      get {
+        DateTime now = DateTime.UtcNow;
+        return now.AddDays(DayOfWeek.Saturday - now.DayOfWeek).Date.Ticks;
+      }
+    }
+
+    public static long MidnightSaturdayIct {
+      get { return MidnightSaturdayUtc - (TimeSpan.TicksPerHour * OFFSET_ICT); }
+    }
+
+    public static long StopwatchNow {
+      get { return System.Diagnostics.Stopwatch.GetTimestamp(); }
+    }
+
+    public static DateTime FromTicks(long ticks) {
+      return new DateTime(ticks, DateTimeKind.Utc);
+    }
+
+    private Time() {
       // Prevent instantiation
     }
   }
--- src/UtilsTest/UtilsTest.cs
+++ src/UtilsTest/UtilsTest.cs
@@ -2,14 +2,34 @@ using System;
 using Xunit;
 
 namespace Utils {
-  public class UtilsTest {
+  public class StringsTest {
     [Fact]
     [Trait("Category", "Unit")]
     public void GetValueTest() {
-      Assert.Equal("", Utils.GetValue("\n\n"));
-      Assert.Equal("value", Utils.GetValue("value"));
-      Assert.Equal("default", Utils.GetValue("\n\n", "default"));
-      Assert.Equal("value", Utils.GetValue("value", "default"));
+      Assert.Equal("", Strings.GetValue("\n\n"));
+      Assert.Equal("value", Strings.GetValue("value"));
+      Assert.Equal("default", Strings.GetValue("\n\n", "default"));
+      Assert.Equal("value", Strings.GetValue("value", "default"));
+    }
+  }
+
+  public class TimeTest {
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TimeTests() {
+      long now = Time.Now;
+      long stopwatch = Time.StopwatchNow;
+      Assert.NotEqual(now, stopwatch);
+      DateTime nowDt = Time.FromTicks(now);
+      Assert.Equal(DateTimeKind.Utc, nowDt.Kind);
+      Assert.Equal(DateTime.UtcNow.Date, nowDt.Date);
+      Assert.Equal(DateTime.UtcNow.Hour, nowDt.Hour);
+      Assert.Equal(DateTime.UtcNow.Date.AddDays(1).Date, Time.FromTicks(Time.MidnightUtc));
+      Assert.Equal(DayOfWeek.Saturday, Time.FromTicks(Time.MidnightSaturdayUtc).DayOfWeek);
+      Assert.Equal(Time.FromTicks(Time.MidnightUtc).AddHours(-1 * Time.OFFSET_ICT),
+        Time.FromTicks(Time.MidnightIct));
+      Assert.Equal(Time.FromTicks(Time.MidnightSaturdayUtc).AddHours(-1 * Time.OFFSET_ICT),
+        Time.FromTicks(Time.MidnightSaturdayIct));
     }
   }
 }
